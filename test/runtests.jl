23ad3127f5b106a28c4d77ee18dd5320550aeedb
Michael F. Herbst <info@michael-herbst.com>
2023-03-26 18:15:48 +0200

    up

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using Pkg
 
 const GROUP = get(ENV, "GROUP", "Core")
+const GROUP_COVERAGE = !isempty(get(ENV, "GROUP_COVERAGE", ""))
 
 if GROUP == "Core"
     @testset "AtomsIO.jl" begin
@@ -13,5 +14,5 @@ if GROUP == "Core"
 else
     subpkg_path = joinpath(dirname(@__DIR__), "lib", GROUP)
     Pkg.develop(PackageSpec(path=subpkg_path))
-    Pkg.test(PackageSpec(name=GROUP, path=subpkg_path))
+    Pkg.test(PackageSpec(name=GROUP, path=subpkg_path), coverage=GROUP_COVERAGE)
 end
