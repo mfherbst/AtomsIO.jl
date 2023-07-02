5aa66ca62d7ae1ea0e98fcd000445386bafcc246
Michael F. Herbst <info@michael-herbst.com>
2023-07-02 14:44:00 +0200

    Add failed files test (#21)

---
+++ test/failed_files.jl
@@ -0,0 +1,21 @@
+using Test
+using AtomsIO
+using Unitful
+using LinearAlgebra
+
+# Tests parsing files, where previously users reported problems
+@testset "Failed files" begin
+@testset "CIF Graphene P6/mmm" begin
+    parsed = load_system("files/graphene.cif")
+    reduced = reduce(hcat, bounding_box(parsed))
+
+    @test maximum(abs, reduced - Diagonal(reduced)) < 1e-12u"Å"
+    @test diag(reduced) ≈ [2.4595, 4.26, 30]u"Å"
+
+    @test atomic_symbol(parsed) == [:C1, :C2, :C3, :C4]
+    @test atomic_number(parsed) == [6, 6, 6, 6]
+    @test parsed[:name] == "Graphene"
+
+
+end
+end
