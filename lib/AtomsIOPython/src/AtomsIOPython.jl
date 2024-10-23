9802910d9e929b9926b92389ec5753e3005e9e4c
Michael F. Herbst <info@michael-herbst.com>
2024-10-23 07:37:34 +0200

    Add parser order mechanics

--- lib/AtomsIOPython/src/AtomsIOPython.jl
+++ lib/AtomsIOPython/src/AtomsIOPython.jl
@@ -2,17 +2,15 @@ module AtomsIOPython
 using Reexport
 @reexport using AtomsIO
 
+export AseParser
 include("ase.jl")
 
-export AseParser
-
-function __init__()
-    # Append python-based parsers to the parser list in AtomsIO.
+function AtomsIO.atomsio_extra_parsers(::AtomsIO.PythonParsers)
+    # Register the python-based parsers with AtomsIO
+    #
     # Note: For reproducibility reasons changing the order of these parsers is a
     # *breaking change* (as it alters the behaviour of AtomsIO.load_system).
-    # Moreover since not all users will want to rely on Python dependencies, it is
-    # crucial that the python-based parsers are *appended* to this list.
-    parsers = AbstractParser[AseParser(), ]
-    append!(AtomsIO.DEFAULT_PARSER_ORDER, parsers)
+    AbstractParser[AseParser(), ]
 end
+
 end
